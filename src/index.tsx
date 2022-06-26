e305bb33ae67fd3a9dff95428949328bf3a98b48
tes7a <ttes7a@gmail.com>
2022-06-26 18:03:43 +0300

    change routes

--- src/index.tsx
+++ src/index.tsx
@@ -5,18 +5,18 @@ import App from './components/app/App';
 import reportWebVitals from './reportWebVitals';
 import {Provider} from "react-redux";
 import {store} from "./bll/store";
-import {BrowserRouter} from "react-router-dom";
+import {HashRouter} from "react-router-dom";
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <BrowserRouter>
+    <HashRouter>
         <Provider store={store}>
             <App/>
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
